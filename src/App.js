a7f5c2ed95025d65cff30b6238f3dddef44303a8
ivanevacic <ivanevacicweb@gmail.com>
2018-02-17 00:35:59 +0100

    Added background color and css to Navigation component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        {/*<Logo />
+        <Logo />
+        {
+        /*
         <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     //  In React we use camel case when we need to use dash this line below. 'justify-content' = 'justifyContent'
     //  'flex-end' => to the right 
     <nav style={{ display: 'flex', justifyContent: 'flex-end'}}>  
-      <p> Sign Out </p>
+      <p className='f3 link dim black underline pa3 pointer'> Sign Out </p>
     </nav>
   );
 }
