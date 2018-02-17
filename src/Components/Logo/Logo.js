404188fa729b2626334e92ec6d26e9f3da0a54ee
ivanevacic <ivanevacicweb@gmail.com>
2018-02-17 01:03:45 +0100

    Installed and implemented 'react-tilt' package for styling

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,9 +1,15 @@
 import React from 'react';
+//  https://www.npmjs.com/package/react-tilt
+import Tilt from 'react-tilt';
+//  Import styling for this component
+import './Logo.css';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-    
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner"> 👽 </div>
+      </Tilt>
     </div>
   );
 }
