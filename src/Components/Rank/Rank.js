4cdef4494b1a5e16f39c92f57b37f316256b1d2b
ivanevacic <ivanevacicweb@gmail.com>
2018-02-17 01:50:26 +0100

    Changed app font

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Rank = () => {
   return (
     <div>
-      
+      <div className='black f3'>
+        { 'Ivan, your current rank is....'  }
+      </div>
+      <div className='red f1'>
+        { '#1'  }
+      </div>
     </div>
   );
 }
