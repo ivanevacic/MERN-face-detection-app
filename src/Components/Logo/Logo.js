e997ce3a5919387e3127bd4813d8d4cad7dfdc38
ivanevacic <ivanevacicweb@gmail.com>
2018-02-17 01:18:12 +0100

    Finished 'Logo' component

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,12 +3,17 @@ import React from 'react';
 import Tilt from 'react-tilt';
 //  Import styling for this component
 import './Logo.css';
+//  Import logo picture URL:'https://icons8.com/icon/48534/mind-map'
+import AIimage from './AI-icon.png';  
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
       <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner pa3"> 
+        {/* Import logo and set padding top 5px */}
+          <img style={{ paddingTop: '5px' }}alt='logo' src={ AIimage }/>
+        </div>
       </Tilt>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//  Import 'tachyons' CSS
+//  Import 'tachyons' CSS URL:'http://tachyons.io/'
 import 'tachyons';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
